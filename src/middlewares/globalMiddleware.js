ae0707771bd948582e53a93f3a72d91643bcba5b
Ali Magdy <166527591+alimagdye@users.noreply.github.com>
2025-05-25 12:03:27 +0300

    Update globalMiddleware.js

--- src/middlewares/globalMiddleware.js
+++ src/middlewares/globalMiddleware.js
@@ -12,5 +12,5 @@ export const globalMiddleware = [
 // error handler middleware for global errors. It should be the last middleware in the app. this only can catch errors that are thrown synchronously.
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
-  res.status(400).json({ msg: "unexpected error" });
+  res.status(400).json({ message: "unexpected error" });
 };
